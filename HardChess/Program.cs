76e2aa8793d9f6b05e33f74390f35eea38aba7ca
suxar <suxar@EGOR>
2026-05-28 17:30:09 +0700

    HardChess in Progress PascalTriangle Done

---
+++ HardChess/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardChess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер шахматной доски: ");
+            int boardSize = Convert.ToInt32(Console.ReadLine());
+            const int cubeSize = 5;
+            for (int boardRow = 0; boardRow < boardSize; boardRow++)
+            {
+                for (int lineInCube = 0; lineInCube < cubeSize; lineInCube++)
+                {
+                    string currentLine = "";
+                    for (int boardCol = 0; boardCol < boardSize; boardCol++)
+                    {
+                        string cubePart = ((boardRow + boardCol) % 2 == 0)
+                            ? new string('#', cubeSize)
+                            : new string(' ', cubeSize);
+                        currentLine += cubePart;
+                    }
+                    Console.WriteLine(currentLine);
+                }
+            }
+        }
+    }
+}
---
+++ PascalTriangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int maxLineLength = n * 4;
+
+            for (int i = 0; i < n; i++)
+            {
+                StringBuilder line = new StringBuilder();
+                long value = 1;
+                for (int j = 0; j <= i; j++)
+                {
+                    line.Append(value).Append("   ");
+                    value = value * (i - j) / (j + 1);
+                }
+
+                string rowString = line.ToString().TrimEnd();
+
+                int indentSize = (maxLineLength - rowString.Length) / 2;
+                string indent = new string(' ', indentSize);
+
+                Console.WriteLine(indent + rowString);
+            }
+        }
+
+    }
+}
